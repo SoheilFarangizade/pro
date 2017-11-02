caac8ba927478412c95bd76707e0c03185a08857
Ali <aliseylaneh@gmail.com>
2017-11-02 16:10:09 +0330

    The Circle Comment Has added to project source

--- Circle.java
+++ Circle.java
@@ -1,6 +1,9 @@
 package ir.javacup.paint;
 
 public class Circle extends Shape {
+    /* the circle has extended of shap becaue there 
+    are some fields in it and 
+    we need them for our shape*/
 	Double redius;
 
 	public Circle(Color c, Pattern p, double r) {
